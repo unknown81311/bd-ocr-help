dcd934e5f906e5e204cf80022da21e614726a6f2
unknown81311 <43104779+unknown81311@users.noreply.github.com>
2022-03-17 20:16:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ client.on("ready", () => {
 });
 
 function getTextFromImage(url) {
+  //using Tesseract to find text in images
   return Tesseract.recognize(url).then(({ data: { text } }) => {
     return text.toLowerCase();
   });
@@ -24,14 +25,14 @@ function checkUrl(u) {
   return r;
 }
 
-const messagesRegexes = {
+const messagesRegexes = {//regexs for error messages
   crash: /betterdiscord seems to have crashed your discord client/g,
   assets: /cannot read property (|.)assets(|.) of undefined/g,
   awkward: /well. this is awkward|error level 9000/g, //|you died
   PFL: /Try removing all your plugins then restarting discord/g,
 };
 
-const fixes = {
+const fixes = {//messages for replying to errors
   assets: `If you are running into an Cannot read property 'assets' of undefined error while trying to install BetterDiscord you are using an outdated version of the installer. Please download the current version from https://betterdiscord.app/`,
   crash: `Go to your plugins folder (look below) and replace ZeresPluginLibrary (file name is 0PluginLibrary) -> https://betterdiscord.app/Download?id=9\nIf that does not work, replace BDFDB (file name is 0BDFDB) -> https://betterdiscord.app/Download?id=59`,
   PFL: `Windows: Press win + r and paste in %appdata%/betterdiscord/plugins\nMac: Go to finder, press cmd + shift + G and paste in ~/Library/Application Support/betterdiscord/plugins\nLinux: Go to your command line and type cd ~/.config/BetterDiscord/plugins`,
@@ -42,14 +43,18 @@ const linkReg =
 
 
 async function getReply(message, regex) {
+  // find what type of error the message is
+  
   if (!message.attachments) return;
 
-  if (message.attachments.first()) {
+  if (message.attachments.first()) {//if error is an attachment
     const ImageText = await getTextFromImage(
       message.attachments.first().attachment
     );
 
     for (let reg = 0; reg < Object.keys(messagesRegexes).length; reg++) {
+      //loop over all erorr regex and test the text provided by getTextFromImage
+      
       if (
         messagesRegexes[Object.keys(messagesRegexes)[reg]].test(
           ImageText.toLowerCase()
@@ -65,17 +70,20 @@ async function getReply(message, regex) {
     let sent = false;
 
     for (let i = 0; i < links.length || !sent; i++) {
-      if (checkUrl(message.content)) {
+      // loop over all urls untill an error is found
+      
+      if (checkUrl(message.content)) {//check if url is real
         sent = true;
         const ImageText = await getTextFromImage(links[i]);
 
         for (let reg = 0; reg < Object.keys(messagesRegexes).length; reg++) {
+          //loop over all erorr regex and test the text provided by getTextFromImage
           if (
             messagesRegexes[Object.keys(messagesRegexes)[reg]].test(
               ImageText.toLowerCase()
             )
           ) {
-            return `<@${message.author.id}>` + fixes[Object.keys(messagesRegexes)[reg]];
+            return `<@${message.author.id}>` + fixes[Object.keys(messagesRegexes)[reg]];// return the fix message and mentioning them
           }
         }
       }
@@ -88,11 +96,13 @@ client.on("messageCreate", async (message) => {
     if (message.author.bot) return;
     if (message.channel.name != "bot-testing") return;
     const msg = message;
-    const reply = await getReply(msg);
-    if (reply) message.reply(reply);
-  } catch (err) {
+    const reply = await getReply(msg);//test if there is an image with getReply
+    if (reply) message.reply(reply);//if getReply is not undefind reply with the fix message returned
+  }
+  catch (err) {
     console.error(err);
   }
 });
 
+//start bot
 client.login('BOT-TOKEN');
