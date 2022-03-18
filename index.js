bad8b15441298be12bc8a5321f60fc6eb1fbe8b2
unknown81311 <43104779+unknown81311@users.noreply.github.com>
2022-03-17 20:07:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -23,42 +23,42 @@ function checkUrl(u) {
   }
   return r;
 }
-async function getReply(message, regex) {
-  const messagesRegexes = {
-    crash: /betterdiscord seems to have crashed your discord client/g,
-    assets: /cannot read property (|.)assets(|.) of undefined/g,
-    awkward: /well. this is awkward|error level 9000/g, //|you died
-    PFL: /Try removing all your plugins then restarting discord/g,
-  };
 
-  const fixes = {
-    assets: `<@${message.author.id}> If you are running into an Cannot read property 'assets' of undefined error while trying to install BetterDiscord you are using an outdated version of the installer. Please download the current version from https://betterdiscord.app/`,
-    crash: `<@${message.author.id}> Go to your plugins folder (look below) and replace ZeresPluginLibrary (file name is 0PluginLibrary) -> https://betterdiscord.app/Download?id=9\nIf that does not work, replace BDFDB (file name is 0BDFDB) -> https://betterdiscord.app/Download?id=59`,
-    PFL: `<@${message.author.id}> Windows: Press win + r and paste in %appdata%/betterdiscord/plugins\nMac: Go to finder, press cmd + shift + G and paste in ~/Library/Application Support/betterdiscord/plugins\nLinux: Go to your command line and type cd ~/.config/BetterDiscord/plugins`,
-    awkward: `<@${message.author.id}> There are a number of plugins that are crashing Discord. Please :check_the_pins: READ THE PINS :check_the_pins: for more information.`,
-  };
+const messagesRegexes = {
+  crash: /betterdiscord seems to have crashed your discord client/g,
+  assets: /cannot read property (|.)assets(|.) of undefined/g,
+  awkward: /well. this is awkward|error level 9000/g, //|you died
+  PFL: /Try removing all your plugins then restarting discord/g,
+};
+
+const fixes = {
+  assets: `If you are running into an Cannot read property 'assets' of undefined error while trying to install BetterDiscord you are using an outdated version of the installer. Please download the current version from https://betterdiscord.app/`,
+  crash: `Go to your plugins folder (look below) and replace ZeresPluginLibrary (file name is 0PluginLibrary) -> https://betterdiscord.app/Download?id=9\nIf that does not work, replace BDFDB (file name is 0BDFDB) -> https://betterdiscord.app/Download?id=59`,
+  PFL: `Windows: Press win + r and paste in %appdata%/betterdiscord/plugins\nMac: Go to finder, press cmd + shift + G and paste in ~/Library/Application Support/betterdiscord/plugins\nLinux: Go to your command line and type cd ~/.config/BetterDiscord/plugins`,
+  awkward: `There are a number of plugins that are crashing Discord. Please :check_the_pins: READ THE PINS :check_the_pins: for more information.`,
+};
+const linkReg =
+      /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/g;
+
+
+async function getReply(message, regex) {
   if (!message.attachments) return;
 
   if (message.attachments.first()) {
     const ImageText = await getTextFromImage(
       message.attachments.first().attachment
     );
-    
+
     for (let reg = 0; reg < Object.keys(messagesRegexes).length; reg++) {
       if (
         messagesRegexes[Object.keys(messagesRegexes)[reg]].test(
           ImageText.toLowerCase()
         )
       ) {
-        return fixes[Object.keys(messagesRegexes)[reg]];
+        return `<@${message.author.id}>` + fixes[Object.keys(messagesRegexes)[reg]];
       }
     }
-
-  }
-  else {
-
-    const linkReg =
-      /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/g;
+  } else {
     const isLinks = linkReg.test(message.content);
     if (!isLinks) return;
     const links = message.content.match(linkReg);
@@ -68,14 +68,14 @@ async function getReply(message, regex) {
       if (checkUrl(message.content)) {
         sent = true;
         const ImageText = await getTextFromImage(links[i]);
-        
+
         for (let reg = 0; reg < Object.keys(messagesRegexes).length; reg++) {
           if (
             messagesRegexes[Object.keys(messagesRegexes)[reg]].test(
               ImageText.toLowerCase()
             )
           ) {
-            return fixes[Object.keys(messagesRegexes)[reg]];
+            return `<@${message.author.id}>` + fixes[Object.keys(messagesRegexes)[reg]];
           }
         }
       }
@@ -87,13 +87,10 @@ client.on("messageCreate", async (message) => {
   try {
     if (message.author.bot) return;
     if (message.channel.name != "bot-testing") return;
-    
     const msg = message;
     const reply = await getReply(msg);
-    
     if (reply) message.reply(reply);
-  } 
-  catch (err) {
+  } catch (err) {
     console.error(err);
   }
 });
