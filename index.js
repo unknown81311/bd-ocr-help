4af5eaf553a8a6f3f6c33e9f86ed0905bdce8875
unknown81311 <43104779+unknown81311@users.noreply.github.com>
2022-03-17 19:15:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Tesseract = require('tesseract.js');
-
-const { Client, Intents } = require('discord.js');
+const {
+    Client,
+    Intents
+} = require('discord.js');
 const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
@@ -12,59 +14,78 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-const messagesRegexes={"assets":/Cannot read property (|.)assets(|.) of undefined/g,"links":/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/g};
+const messagesRegexes = {
+    "crash": /BetterDiscord seems to have crashed your Discord client/g,
+    "assets": /Cannot read property (|.)assets(|.) of undefined/g,
+    "awkward": /well. this is awkward/g,
+    "PFL": /Try removing all your plugins then restarting Discord/g,
+    "links": /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/g
+};
 
 function getTextFromImage(url) {
-  return Tesseract.recognize(url)
-    .then(({ data: { text } }) => {
-        return text;
-    })
+    return Tesseract.recognize(url)
+        .then(({
+            data: {
+                text
+            };
+        }) => {
+            return text.toLowerCase();
+        })
 }
 
-function checkUrl(u){
-  let r=true
-  try{
-    let x = new URL(u);
-  }catch(e){
-    r=false
-  }
-  return r;
+function checkUrl(u) {
+    let r = true
+    try {
+        let x = new URL(u);
+    } catch (e) {
+        r = false
+    }
+    return r;
+}
+async function getReply(message, regex) {
+    const fixes = {
+        assets: `<@${message.author.id}> If you are running into an Cannot read property 'assets' of undefined error while trying to install BetterDiscord you are using an outdated version of the installer. Please download the current version from https://betterdiscord.app/`,
+        crash: `<@${message.author.id}> Go to your plugins folder (look below) and replace ZeresPluginLibrary (file name is 0PluginLibrary) -> https://betterdiscord.app/Download?id=9\nIf that does not work, replace BDFDB (file name is 0BDFDB) -> https://betterdiscord.app/Download?id=59`,
+        PFL: `<@${message.author.id}> Windows: Press win + r and paste in %appdata%/betterdiscord/plugins\nMac: Go to finder, press cmd + shift + G and paste in ~/Library/Application Support/betterdiscord/plugins\nLinux: Go to your command line and type cd ~/.config/BetterDiscord/plugins`,
+        awkward: `<@${message.author.id}> There are a number of plugins that are crashing Discord. Please :check_the_pins: READ THE PINS :check_the_pins: for more information.`
+    };
+    if (messagesRegexes[regex].test(message.content)) {
+        return fixes[regex];
+    }
+    if (!message.attachments) return;
+
+    if (message.attachments.first()) {
+        const ImageText = await getTextFromImage(message.attachments.first().attachment);
+        if (messagesRegexes[regex].test(ImageText)) {
+            return fixes[regex];
+        }
+    } else {
+        const isLinks = messagesRegexes.links.test(message.content);
+        if (!isLinks) return;
+        const links = message.content.match(messagesRegexes.links);
+        let sent = false;
+        for (let i = 0; i < links.length || (!sent); i++) {
+            if (checkUrl(message.content)) {
+                sent = true;
+                const ImageText = await getTextFromImage(links[i]);
+                if (messagesRegexes[regex].test(ImageText)) {
+                    return fixes[regex];
+                }
+            }
+        }
+    }
 }
 
 client.on('messageCreate', async message => {
-  try{
-    if (message.author.bot) return;
-    if(message.channel.name!='bot-testing') return;
-    const messageReply = `<@${message.author.id}> If you are running into an Cannot read property 'assets' of undefined error while trying to install BetterDiscord you are using an outdated version of the installer. Please download the current version from https://betterdiscord.app/`
-    if(messagesRegexes.assets.test(message.content)){
-      message.reply(messageReply)
-      return
+    try {
+        if (message.author.bot) return;
+        if (message.channel.name != 'bot-testing') return;
+        const msg = message;
+        const reply = await getReply(msg, 'crash') || await getReply(msg, 'assets') || await getReply(msg, 'awkward') || await getReply(msg, 'PFL');
+        if (reply) message.reply(reply);
+    } catch (err) {
+        console.error(err);
     }
-    if (message.attachments.first()){
-      const ImageText = await getTextFromImage(message.attachments.first().attachment);
-      if(messagesRegexes.assets.test(ImageText)){
-        message.reply(messageReply)
-      }
-    }else{
-      const isLinks=messagesRegexes.links.test(message.content)
-      if(!isLinks)return;
-      const links=message.content.match(messagesRegexes.links)
-      console.log(links);
-      let sent=false;
-      for(let i=0;i<links.length||(!sent);i++){
-        if(checkUrl(message.content)){
-          sent=true;
-          const ImageText = await getTextFromImage(links[i]);
-          if(messagesRegexes.assets.test(ImageText)){
-            message.reply(messageReply)
-            return;
-          }
-        }
-      }
-    }
-  }catch(err){
-    console.error(err);
-  }
 });
 
 client.login('BOT-TOKEN');
